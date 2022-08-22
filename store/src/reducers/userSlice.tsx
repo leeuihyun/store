956890a64bb084a2f131a3f5101e0a30ac7af9e7
leeuihyun <meta212121@naver.com>
2022-08-22 20:09:17 +0900

    유저 슬라이스 찜하기 비동기 로직 타입 설정 및 선언

--- store/src/reducers/userSlice.tsx
+++ store/src/reducers/userSlice.tsx
@@ -12,12 +12,13 @@ const initialState: AllState = {
     wishLoading: false,
     wishDone: false,
     wishError: null,
+    wishData: [],
     cancelLoading: false,
     cancelDone: false,
     cancelError: null,
 };
 export const logIn = createAsyncThunk(
-    "user",
+    "logIn",
     async (data, { rejectWithValue }) => {
         try {
             const res = await axios;
@@ -26,11 +27,55 @@ export const logIn = createAsyncThunk(
         }
     }
 );
+
+export const wish = createAsyncThunk("wish", async (data: number) => {
+    try {
+        const res = await axios.get(
+            `https://fakestoreapi.com/products/${data}`
+        );
+        console.log(res.data);
+        return res.data;
+    } catch (error) {
+        console.error(error);
+    }
+});
 const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {},
-    extraReducers: {},
+    extraReducers: {
+        [logIn.pending.type]: (state, action: PayloadAction<string>) => {
+            state.logInLoading = true;
+            state.logInDone = false;
+            state.logInError = null;
+        },
+        [logIn.fulfilled.type]: (state, action: PayloadAction<string>) => {
+            state.logInLoading = false;
+            state.logInDone = true;
+            state.logInError = null;
+        },
+        [logIn.rejected.type]: (state, action: PayloadAction<string>) => {
+            state.logInLoading = false;
+            state.logInDone = false;
+            state.logInError = action.payload;
+        },
+        [wish.pending.type]: (state, action: PayloadAction<object>) => {
+            state.wishLoading = true;
+            state.wishDone = false;
+            state.wishError = null;
+        },
+        [wish.fulfilled.type]: (state, action: PayloadAction<object>) => {
+            state.wishLoading = false;
+            state.wishDone = true;
+            state.wishError = null;
+            state.wishData = state.wishData.concat(action.payload);
+        },
+        [wish.rejected.type]: (state, action: PayloadAction<object>) => {
+            state.wishLoading = false;
+            state.wishDone = false;
+            state.wishError = action.payload;
+        },
+    },
 });
 export const userSliceActions = userSlice.actions;
 export default userSlice.reducer;
