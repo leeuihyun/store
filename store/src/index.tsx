6f6f0ca48dd8ef7eff3eeec441cec5fa3ec26150
leeuihyun <meta212121@naver.com>
2022-08-22 20:09:38 +0900

    Provider 설정

--- store/src/index.tsx
+++ store/src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import store from "./store/store";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
