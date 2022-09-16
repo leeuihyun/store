56d50f1e55503c473668f4ccdfd24bea1669931e
leeuihyun <meta212121@naver.com>
2022-09-16 17:53:28 +0900

    image Component d.ts 수정 후 css 반응형

---
+++ store/src/subComponents/SecondImageDiv.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { ImageDivStyle } from "../styled/StyledCollection";
+import img from "../images/filmstrip-gc7031bada_1920.jpg";
+
+function SecondImageDiv() {
+    return <ImageDivStyle imgSrc={img}>hi</ImageDivStyle>;
+}
+
+export default SecondImageDiv;
