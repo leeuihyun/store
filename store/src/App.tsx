e43645179ae46531f7cb71d7be2fbfb3fea36a4d
leeuihyun <meta212121@naver.com>
2022-09-19 18:04:00 +0900

    app.tsx

--- store/src/App.tsx
+++ store/src/App.tsx
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import LogInPage from "./pages/LogInPage";
 import PerfumePage from "./pages/PerfumePage";
+import ManPerfumePage from "./pages/ManPerfumePage";
+import GirlPerfumePage from "./pages/GirlPerfumePage";
+import BothPerfumePage from "./pages/BothPerfumePage";
 
 function App() {
     return (
@@ -10,6 +13,9 @@ function App() {
             <Route path="/" element={<MainPage />}></Route>
             <Route path="login" element={<LogInPage />}></Route>
             <Route path="perfume" element={<PerfumePage />}></Route>
+            <Route path="/girl" element={<GirlPerfumePage />}></Route>
+            <Route path="/man" element={<ManPerfumePage />}></Route>
+            <Route path="/both" element={<BothPerfumePage />}></Route>
         </Routes>
     );
 }
